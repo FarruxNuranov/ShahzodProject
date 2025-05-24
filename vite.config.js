0ed191c2bee0f77e7d2d515b078fa49ee7968022
Farrux Nuranov <farruxnuranov2001@gmail.com>
2025-05-24 13:22:49 +0500

    fara

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
+import { fileURLToPath, URL } from 'url'
+
+// превратить import.meta.url в путь к папке
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')  // <— вот так
+      // теперь @ точно смотрит в src вашего проекта
+      '@': path.resolve(__dirname, 'src'),
+      // или можно сразу так:
+      // '@': fileURLToPath(new URL('./src', import.meta.url))
     },
     extensions: ['.js', '.jsx', '.ts', '.tsx']
   }
