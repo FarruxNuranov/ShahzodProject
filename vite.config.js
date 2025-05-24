76fe65327b630013fd760db7a8cf7e1cb94d51b6
Farrux Nuranov <farruxnuranov2001@gmail.com>
2025-05-24 13:22:10 +0500

    fara

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   resolve: {
-    alias: { '@': '/src' }
-   }
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')  // <— вот так
+    },
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  }
 })
